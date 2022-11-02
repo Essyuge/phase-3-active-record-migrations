4745e3db094de4dd8da3bd36e7cc23f640c4e6f0
Essyuge <ugeesther@gmail.com>
2022-11-02 08:08:15 +0300

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,33 @@
 # add the Artist class here
+# db/migrate/20210716095220_create_artists.rb
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/artists.sqlite"
+)
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT,
+  favorite_flower TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+class Artist < ActiveRecord::Base
+end
+
+# class CreateArtists < ActiveRecord::Migration[6.1]
+#     def change
+#       create_table :artists do |t|
+#         t.string :name
+#         t.string :genre
+#         t.integer :age
+#         t.string :hometown
+#         # the id column is gemerated automatically for every table! no need to specify it here.
+#       end
+#     end
+#   end
